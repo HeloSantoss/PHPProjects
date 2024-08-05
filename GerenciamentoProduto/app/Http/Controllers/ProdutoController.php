54468d0ac9032c4a04d505f82a2d4d9720d76ebc
HeloSantoss <heloisasantoslindona07@gmail.com>
2024-08-05 16:29:11 -0300

    05/08

---
+++ GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto; // Certifique-se de importar o modelo Produto
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preço' => 'required|numeric',
+            'quantidade' => 'required|integer',
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')
+                         ->with('success', 'Produto criado com sucesso.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $produto = Produto::findOrFail($id); // Obtenha o produto pelo ID
+        return view('produtos.show', compact('produto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $produto = Produto::findOrFail($id); // Obtenha o produto pelo ID
+        return view('produtos.edit', compact('produto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preço' => 'required|numeric',
+            'quantidade' => 'required|integer',
+        ]);
+
+        $produto = Produto::findOrFail($id); // Obtenha o produto pelo ID
+        $produto->update($request->all());
+
+        return redirect()->route('produtos.index')
+                         ->with('success', 'Produto atualizado com sucesso.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $produto = Produto::findOrFail($id); // Obtenha o produto pelo ID
+        $produto->delete();
+
+        return redirect()->route('produtos.index')
+                         ->with('success', 'Produto deletado com sucesso.');
+    }
+}
--- GerenciamentoProduto/routes/web.php
+++ GerenciamentoProduto/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProdutoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('produtos'),ProdutoController::class);
